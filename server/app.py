a04dfa5c4be2faacbb7b6e6b24fd29c209a13f18
Ben McCarthy <bmccarthy90@gmail.com>
2024-09-14 12:55:41 -0400

    tests pass

--- server/app.py
+++ server/app.py
@@ -11,7 +11,35 @@ def index():
 @app.route('/print/<string:parameter>')
 def print_string(parameter):
     print(parameter)
-    return 200
+    return parameter
+
+@app.route('/count/<string:parameter>')
+def count(parameter):
+    count_range = range(int(parameter))
+    count_list = ""
+    for num in count_range:
+        count_list = f'{count_list}{num}\n' 
+       
+    print(count_list)
+    return count_list
+
+@app.route('/math/<int:num1>/<string:operation>/<int:num2>')
+def math(num1, num2, operation):
+    print(num1, num2, operation)
+    num1_num = int(num1)
+    num2_num = int(num2)
+    if operation == "+":
+        return f"{num1_num + num2_num}"
+    elif operation == "-":
+        return f"{num1_num - num2_num}"
+    elif operation == "*":
+        return f"{num1_num * num2_num}"
+    elif operation == "div":
+        return f"{num1_num / num2_num}"
+    elif operation == "%":
+        return f"{num1_num % num2_num}"
+    else:
+        return 'invalid'
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
